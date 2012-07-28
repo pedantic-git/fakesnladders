0168110959e873db5ff97bb179bd3a66fab37fb1
Rich Daley <git@richd.me>
2012-07-28 15:25:29 +0100

    Add stub topic model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120728120328) do
+ActiveRecord::Schema.define(:version => 20120728142351) do
+
+  create_table "topics", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
