315b39edfa9d02a831d3de0411dd4abb567273c1
Rich Daley <git@richd.me>
2012-07-28 16:56:52 +0100

    Add avatar_url to user models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120728152035) do
+ActiveRecord::Schema.define(:version => 20120728153925) do
 
   create_table "choices", :force => true do |t|
     t.integer  "topic_id"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(:version => 20120728152035) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "position"
+    t.string   "avatar_url"
   end
 
 end
