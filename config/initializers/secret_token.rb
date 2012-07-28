8da7415a45ee5f7a7c253fd5dd579cd9e02be4c7
Rich Daley <git@richd.me>
2012-07-28 12:59:06 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fakesnladders::Application.config.secret_token = '1e55a3944ba6e3533dcaea262262cb91e6696fa13083c7080728dbf02a7dbceaff69d4fcefcf394cfc268f31673518c2fd4565a6ef828f425dd05beaebe537bd'
